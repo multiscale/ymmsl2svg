64b2f23221c2e622dc742abfe9b679b645908926
Maarten Sebregts <msebregts@ignitioncomputing.com>
2026-04-20 13:26:06 +0200

    Draw ports for components

--- src/ymmsl2svg/settings.py
+++ src/ymmsl2svg/settings.py
@@ -6,6 +6,11 @@ class Settings:
     debug: bool = True
     # Component style
     component_border: float = 2
+    """Border width of a component"""
+    port_size: float = 7  # TODO: rescale port sizes based on this value?
+    """Size of a port"""
+    port_margin: float = 10
+    """Size between the centers of two ports"""
 
 
 # Singleton object for global settings
